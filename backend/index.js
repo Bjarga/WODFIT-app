1a0b8d17df14184f42a82db67cd9d6ddcb1981a5
Bjarga <85278388+Bjarga@users.noreply.github.com>
2024-05-23 07:54:36 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -227,10 +227,9 @@ app.put("/user/:id", upload.single("profilePicture"), async (req, res) => {
   }
 });
 
-if (process.env.NODE_ENV !== "test") {
+
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
-}
 
 module.exports = app;
